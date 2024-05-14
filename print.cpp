f70e7d17aee21b401c3be1ce39ccb23f5eb5a22f
prince-aethelred <29713857+prince-aethelred@users.noreply.github.com>
2024-05-13 20:22:08 -0400

    Update print.cpp

--- print.cpp
+++ print.cpp
@@ -4,60 +4,75 @@
 #include <type_traits>
 #include <map>
 #include <unordered_map>
+// #include <concepts>
+/*
+template <typename Type>
+concept is_container = requires(Type t) {
+    typename Type::value_type;
+};*/
 
 // Typeype trait to check if a type is a container
 template <typename Type, typename _ = void>
-struct is_container : std::false_type {};
+struct is_container : std::false_type
+{
+};
 
 template <typename Type>
 struct is_container<
-        Type,
-        std::conditional_t<
-            false,
-            typename Type::value_type,
-            void
-        >
-    > : std::true_type {};
+    Type,
+    std::conditional_t<
+        false,
+        typename Type::value_type,
+        void>> : std::true_type
+{
+};
 
 template <typename Type>
-struct is_map {
+struct is_map
+{
     static const bool value = false;
 };
 
 template <typename Key, typename Value>
-struct is_map<std::map<Key, Value>> {
+struct is_map<std::map<Key, Value>>
+{
     static const bool value = true;
 };
 
 template <typename Key, typename Value>
-struct is_map<std::unordered_map<Key, Value>> {
+struct is_map<std::unordered_map<Key, Value>>
+{
     static const bool value = true;
 };
 
-
-
-    // Overload of << operator for containers
-    // Requires the container to have begin() and end() methods
-    // Requires the container's value_type to have an overloaded << operator
-    template <typename Type>
-    typename std::enable_if<is_container<Type>::value, std::ostream&>::type
-    operator<<(std::ostream& output_stream, const Type& input_container) {
-        output_stream << "[";
-        for (auto it = input_container.begin(); it != input_container.end(); ++it) {
-            if (it != input_container.begin()) {
-                output_stream << ", ";
-            }
-            //output_stream << *it;
-            if constexpr (is_map<Type>::value) {
-                output_stream << it->first << ": ";
-                output_stream << it->second;
-            } else {
-                output_stream << *it;
-            }
+// Overload of << operator for containers
+// Requires the container to have begin() and end() methods
+// Requires the container's value_type to have an overloaded << operator
+template <typename Type>
+typename std::enable_if<is_container<Type>::value, std::ostream &>::type
+operator<<(std::ostream &output_stream, const Type &input_container)
+{
+    output_stream << "[";
+    for (auto it = input_container.begin(); it != input_container.end(); ++it)
+    {
+        if (it != input_container.begin())
+        {
+            output_stream << ", ";
         }
-        output_stream << "]";
-        return output_stream;
+        // output_stream << *it;
+        if constexpr (is_map<Type>::value)
+        {
+            output_stream << it->first << ": ";
+            output_stream << it->second;
+        }
+        else
+        {
+            output_stream << *it;
+        }
     }
+    output_stream << "]";
+    return output_stream;
+}
 
 /**
     template <template <typename, typename...> class ContainerType, typename ValueType, typename... Args>
@@ -76,23 +91,25 @@ struct is_map<std::unordered_map<Key, Value>> {
         os << "]";
         return os;
     }
-    
+
 */
-template<typename Type>
-void print(Type && input) {
+template <typename Type>
+void print(Type &&input)
+{
     std::cout << input;
 }
 
-template<typename Type, typename... Args>
-void print(Type && input, Args&&... args) {
+template <typename Type, typename... Args>
+void print(Type &&input, Args &&...args)
+{
     std::cout << input << " ";
     print(std::forward<Args>(args)...);
 }
 
-
-auto main() -> int {
+auto main() -> int
+{
     std::vector<int> v = {1, 2, 3, 4, 5};
-    //std::cout << v << std::endl;
+    // std::cout << v << std::endl;
     print(v, "Hello", 1, 2.0, 3.0f);
 
     print("\n");
@@ -108,7 +125,7 @@ auto main() -> int {
     int num = 5;
     int *num_ptr = &num;
 
-    int (*num_ptr2) = &num;
+    int(*num_ptr2) = &num;
 
     print("\n", "num: ", num, "*num_ptr: ", *num_ptr, "*num_ptr2: ", (*num_ptr2), "\n");
     return 0;
